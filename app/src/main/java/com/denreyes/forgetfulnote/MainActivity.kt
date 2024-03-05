57145587ed9b6115321f77d3ecf4a38e0414fd23
Densho <denj.rey@gmail.com>
2024-03-04 22:46:50 -0500

    Dialog UI

--- app/src/main/java/com/denreyes/forgetfulnote/MainActivity.kt
+++ app/src/main/java/com/denreyes/forgetfulnote/MainActivity.kt
@@ -13,18 +13,25 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,15 +57,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ForgetfulNoteApp() {
+    var addNoteTitle by remember { mutableStateOf("") }
+    var addNoteText by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
     Column(modifier = Modifier
         .fillMaxHeight()
         .padding(8.dp)
     ) {
         Button(modifier = Modifier.fillMaxWidth(),
-            onClick = { /*TODO*/ }) {
+            onClick = { showDialog = true }) {
             Text(text = "Add Note")
         }
         ForgetfulNoteItem()
+    }
+
+    if (showDialog) {
+        AlertDialog(onDismissRequest = { showDialog = false },
+            confirmButton = {
+                Row (modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Button(onClick = { showDialog = false }) {
+                        addNoteTitle = ""
+                        addNoteText = ""
+                        Text(text = "Add")
+                    }
+                    Button(onClick = { showDialog = false }) {
+                        Text(text = "Cancel")
+                    }
+                }
+            },
+            title = { Text(text = "Add Note") },
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = addNoteTitle,
+                        placeholder = { Text("Title") },
+                        onValueChange = { addNoteTitle = it },
+                        singleLine = true
+                    )
+                    OutlinedTextField(
+                        value = addNoteText,
+                        placeholder = { Text("Body") },
+                        onValueChange = { addNoteText = it },
+                        modifier = Modifier.height(100.dp)
+                    )
+                }
+            }
+        )
     }
 }
 
@@ -73,7 +118,9 @@ fun ForgetfulNoteItem() {
         ),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Column(modifier = Modifier.padding(8.dp).fillMaxWidth(0.7f)) {
+        Column(modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(0.7f)) {
             Text(text = "Title")
             Text(text = "Description")
         }
